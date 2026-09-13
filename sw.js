686a1aad774e23ccaf7fe4a5258bcbe6aad4d39e
ApZero <stefan_kufeld@hotmail.com>
2026-09-13 13:49:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'concursos-shell-v11';
+const CACHE = 'concursos-shell-v12';
 const SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png'];
 
 self.addEventListener('install', (e) => {
